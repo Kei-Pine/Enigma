ee0fc3efc954729ea424bf06f8af4ca96439b725
you dont know me <80095166+Kei-Pine@users.noreply.github.com>
2023-01-29 11:34:50 +0900

    print the length of letters in the output file
    also, expanded the maximam size of input and out put files

--- enicmd4.c
+++ enicmd4.c
@@ -3,24 +3,24 @@
 int main(int argc, char *argv[]){
 		
         FILE *fin, *fout;
-		int x[100],i;
+		int x[1000],i;
         if(argc!=3)puts("wrong cmd!\nformat::cmdname inputFile outputFile");
         if((fin=fopen(argv[1],"r"))== NULL){
                 printf("couldnt open\n");
                 return 1;
         }
-		if((fout=fopen(argv[2],"w"))==NULL){
+	if((fout=fopen(argv[2],"w"))==NULL){
                 printf("couldnt open %s\n",argv[2]);
                 return 1;
         }
 
 
-		for(i=0;i<100;i++){
+	for(i=0;i<1000;i++){
             x[i]=fgetc(fin);
             if(x[i]==EOF)break;
 		}
 		
-		for(i=0;i<100;i++){
+	for(i=0;i<1000;i++){
             if(x[i]==EOF)break;
             fprintf(fout,"%c",x[i]+1);
             
@@ -29,5 +29,6 @@ int main(int argc, char *argv[]){
 
         fclose(fin);
         fclose(fout);
-                return 0;
+        printf("file size is %d",i);
+        return 0;
         }
